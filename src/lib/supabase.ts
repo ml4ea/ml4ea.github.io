ca034eed94a4341bf908d64e9e792e8f2d66f73b
Yan Jin <yjin@usc.edu>
2026-07-21 21:10:19 -0700

    Make email sign-in work across browsers

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -14,7 +14,7 @@ export function getSupabaseClient() {
     client = createClient(supabaseUrl, supabasePublishableKey, {
       auth: {
         detectSessionInUrl: true,
-        flowType: 'pkce',
+        flowType: 'implicit',
         persistSession: true,
       },
     });
